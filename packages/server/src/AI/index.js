ddf21a62132aa4c9483d67a411d04b997db613e6
sergey.suslov <sergey.suslov.nsk@gmail.com>
2019-12-29 09:28:32 +0700

    docs: ai calculate turn and Game findSeq

--- packages/server/src/AI/index.js
+++ packages/server/src/AI/index.js
@@ -1,5 +1,10 @@
 import _ from 'lodash'
 
+/**
+ * Calculates possible turn
+ * @param {Array<Array<Number>>} field Array or rows
+ * @return {{row: Number, col: Number}|null} Object that holds row and col of possible turn
+ */
 export const calculateTurn = field => {
   const index = _.flatten(field).indexOf(0)
 
